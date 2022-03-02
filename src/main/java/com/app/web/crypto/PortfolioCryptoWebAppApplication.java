9c1e9175ddcc2e8a73100960dcfc10decfca673f
Eduardo Martinez <45455445+Jalapinho1@users.noreply.github.com>
2022-03-02 23:36:30 +0100

    Update PortfolioCryptoWebAppApplication.java

--- src/main/java/com/app/web/crypto/PortfolioCryptoWebAppApplication.java
+++ src/main/java/com/app/web/crypto/PortfolioCryptoWebAppApplication.java
@@ -8,6 +8,7 @@ public class PortfolioCryptoWebAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PortfolioCryptoWebAppApplication.class, args);
+        //comment
     }
 
 }
